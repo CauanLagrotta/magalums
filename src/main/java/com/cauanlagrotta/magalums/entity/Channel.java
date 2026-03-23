da2abd34a015ba2d375c72a8d233e6ce294de9a2
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-03-23 10:12:50 -0300

    feat: add entities

---
+++ src/main/java/com/cauanlagrotta/magalums/entity/Channel.java
@@ -0,0 +1,30 @@
+package com.cauanlagrotta.magalums.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_channels")
+public class Channel {
+  @Id
+  private Long channelId;
+
+  private String description;
+
+  public Long getChannelId() {
+    return channelId;
+  }
+
+  public void setChannelId(Long channelId) {
+    this.channelId = channelId;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
