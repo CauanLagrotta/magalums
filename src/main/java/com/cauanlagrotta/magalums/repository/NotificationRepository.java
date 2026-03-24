e7f4fd68adcc5ada5e2897b459a7de51983dc54d
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-03-23 21:16:21 -0300

    feat: add task scheduler for notification processing

--- src/main/java/com/cauanlagrotta/magalums/repository/NotificationRepository.java
+++ src/main/java/com/cauanlagrotta/magalums/repository/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.cauanlagrotta.magalums.repository;
 
 import com.cauanlagrotta.magalums.entity.Notification;
+import com.cauanlagrotta.magalums.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+  List<Notification> findByStatusInAndDateTimeBefore(List<Status> status, LocalDateTime dateTime);
 }
--- src/main/java/com/cauanlagrotta/magalums/service/NotificationService.java
+++ src/main/java/com/cauanlagrotta/magalums/service/NotificationService.java
@@ -4,9 +4,13 @@ import com.cauanlagrotta.magalums.controller.dto.ScheduleNotificationDTO;
 import com.cauanlagrotta.magalums.entity.Notification;
 import com.cauanlagrotta.magalums.entity.Status;
 import com.cauanlagrotta.magalums.repository.NotificationRepository;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class NotificationService {
@@ -30,5 +34,21 @@ public class NotificationService {
       notification.get().setStatus(Status.Values.CANCELLED.toStatus());
       notificationRepository.save(notification.get());
     }
+  }
+
+  public void checkAndSend(LocalDateTime dateTime){
+    var notifications = notificationRepository.findByStatusInAndDateTimeBefore(List.of(
+          Status.Values.PENDING.toStatus(),
+          Status.Values.ERROR.toStatus()
+        ), dateTime);
+
+    notifications.forEach(sendNotification());
+  }
+
+  private Consumer<Notification> sendNotification() {
+    return n -> {
+      n.setStatus(Status.Values.SUCCESS.toStatus());
+      notificationRepository.save(n);
+    };
   }
 }
